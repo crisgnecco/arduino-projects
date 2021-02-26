31830c65b99c14577e4df3e5f6e0b3c1524294a7
Cristian Gnecco <42185984+crisgnecco@users.noreply.github.com>
2021-02-26 11:32:54 -0300

    Add files via upload

--- RiegoEnBaseAHumedad.ino/RiegoEnBaseAHumedad.ino.ino
+++ RiegoEnBaseAHumedad.ino/RiegoEnBaseAHumedad.ino.ino
@@ -1,38 +1,46 @@
 
+// -----------------------------------------------------------------
+/*** cada 12hs chequea si la tierra esta humeda y se debe regar ***/
+// -----------------------------------------------------------------
+
+
 int humValue = 0;
 int humidity;
 int solePin = 7;
 int sensorhumidity = A1;
+int msecs;
 
 void setup() {
   pinMode(solePin, OUTPUT);
-  Serial.begin(9600); 
+  Serial.begin(9600);
+
+  int hours = 12;
+  msecs = hours * 3600000;
 }
 
 void loop() {
 
   humValue = analogRead(sensorhumidity);
-  
-  delay(1500);
 
   humidity = (100.0 * humValue) / 1024;
   Serial.print("humidity:");
   Serial.println(humidity);
   Serial.println("=======================================================================");
-  
-  // ----------------------------------------------------
-  // Chequeo si debo regar
-  // ----------------------------------------------------
-  
-  if( humidity > 70 ) {
-    
+
+  if ( humidity > 70 ) {
+
     Serial.println("Humedad baja, se activa manguera de riego.");
     digitalWrite(solePin, HIGH);
-    
-  } else {
+
+    /* espero tiempo de riego, 10 secs */
+    delay(10000);
     digitalWrite(solePin, LOW);
+
   }
-  
+
+  /*** espero n hs  ***/
+  delay(msecs);
+
   Serial.println("=======================================================================");
 
 }
